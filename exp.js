5458959b44aab35f8c0b63aaacd21e4783fddc0b
rahatsheikhlion <135815469+rahatsheikhlion@users.noreply.github.com>
2023-08-01 00:04:50 +0600

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,27 @@
+$(document). ready (function(){
+
+
+   
+    $(".navigation-Burger i").click(function(){		
+	  $(".navigation-bar-for-tablets") .slideToggle('slow')
+
+    });
+
+
+     $(".navigation-Burger i").click(function(){		
+	$(".navigation-bar-for-phones") .slideToggle('slow')
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
